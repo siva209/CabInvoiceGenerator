73b20838cbf3f7a983a1e71f9e9633b65e581ae3
siva209 <sivanarayana209@gmail.com>
2021-06-18 07:57:48 +0530

    Added:Functionality to calculate total fare for multiple rides

--- src/main/java/com/bridgelabz/invoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoiceGenerator/InvoiceGenerator.java
@@ -11,4 +11,11 @@ public class InvoiceGenerator {
 		}
 		return totalfare;
 	}
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for(Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
 }
