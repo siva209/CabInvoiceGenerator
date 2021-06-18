96016d27ca6c77e842d59a60027a6b825c7542f4
siva209 <sivanarayana209@gmail.com>
2021-06-18 10:46:37 +0530

    Added:Repository to keep track of the rides

--- src/main/java/com/bridgelabz/invoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/invoiceGenerator/InvoiceGenerator.java
@@ -4,6 +4,11 @@ public class InvoiceGenerator {
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
+	private RideRepository rideRepository;
+	public InvoiceGenerator() {
+		rideRepository = new RideRepository();
+	}
+	
 	public double calculateFare(double distance, int time) {	
 		double totalfare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 		if(totalfare < MINIMUM_FARE) {
@@ -20,6 +25,13 @@ public InvoiceSummary calculateFare(Ride[] rides) {
 	}
 	return new InvoiceSummary(rides.length, totalFare);
 	
+}
+public void addRides(String userId, Ride[] rides) {
+	rideRepository.addRides(userId, rides);
+}
+
+public InvoiceSummary getInvoiceSummary(String userId) {
+	return this.calculateFare(rideRepository.getRides(userId));
 }
 	
 }
