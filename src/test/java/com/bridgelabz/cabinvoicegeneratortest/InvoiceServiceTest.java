a3828d79cc48b1dbccdb1106a4db8d0d8078c884
siva209 <sivanarayana209@gmail.com>
2021-06-18 07:59:32 +0530

    Added:Test case for calculating total fare for multiple rides

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/InvoiceServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import com.bridgelabz.invoiceGenerator.InvoiceGenerator;
+import com.bridgelabz.invoiceGenerator.Ride;
 
 public class InvoiceServiceTest {
 	@Test
@@ -22,6 +23,14 @@ public class InvoiceServiceTest {
 		int time = 2;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(5, fare, 0.0);
+	}
+	@Test
+	public void givenMultipleRides_shouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(3.0, 5),
+				         new Ride(0.1, 2)
+		               };
+		assertEquals(40, invoiceGenerator.calculateFare(rides), 0.0);	
 	}
 }
 
