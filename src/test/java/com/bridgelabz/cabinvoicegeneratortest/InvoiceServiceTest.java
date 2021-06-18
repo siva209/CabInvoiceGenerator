8a968a62761dc92ad7739feb5d47cd89118e9e5a
siva209 <sivanarayana209@gmail.com>
2021-06-18 10:17:29 +0530

    Refactored:Test to check for Invoice Summary

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/InvoiceServiceTest.java
@@ -27,5 +27,15 @@ public class InvoiceServiceTest {
 		Assert.assertEquals(5, fare, 0.0);
 	}
 	
+	@Test
+	public void givenMultipleRides_shouldReturnInvoiceSummary() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5),
+				         new Ride(0.1, 1)
+		               };
+		InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary=new InvoiceSummary(2, 30.0);
+		Assert.assertEquals(expectedInvoiceSummary,summary);	
+	}
 }
 
